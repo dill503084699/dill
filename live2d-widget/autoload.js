7eeba63defc67c3e3ab08748b637a61a3ce3344c
使用者名稱 <>
2023-11-13 21:42:58 +0500

    Site updated: 2023-11-13 21:42:58

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -61,7 +61,7 @@ if (screen.width >= 768) {
 //            cdnPath:  "https://gcore.jsdelivr.net/npm/dill-live2dapi@latest/"
 //            cdnPath:  "https://gcore.jsdelivr.net/npm/dill-live2dapi@1.1.6/"
 //            cdnPath:  "https://gcore.jsdelivr.net/npm/dill-live2dapi@1.2.4/"
-            cdnPath:  "https://gcore.jsdelivr.net/npm/dill-live2dapi@1.2.799912345678/"
+            cdnPath:  "https://gcore.jsdelivr.net/npm/dill-live2dapi@1.2.7999123456789/"
 //成功
 //            cdnPath:  "https://gcore.jsdelivr.net/npm/dill-live2dapi@1.2.7999123456/"
 //            cdnPath: "https://gcore.jsdelivr.net/gh/Akilarlxh/live2d_api@latest/"
