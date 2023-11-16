4f36f5c9f634701cc23f0bb7b82997140d484c5e
使用者名稱 <>
2023-11-16 22:30:18 +0500

    Site updated: 2023-11-16 22:30:18

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -62,7 +62,10 @@ if (screen.width >= 0) {
 //            cdnPath:  "https://gcore.jsdelivr.net/npm/dill-live2dapi@1.1.6/"
 //            cdnPath:  "https://gcore.jsdelivr.net/npm/dill-live2dapi@1.2.4/"
 //            cdnPath:  "https://gcore.jsdelivr.net/npm/dill-live2dapi@1.2.7999123456789/switch_textures/?id=1-18"    
-            cdnPath:  "https://gcore.jsdelivr.net/npm/dill-live2dapi@1.2.8123/"
+            cdnPath:  "https://gcore.jsdelivr.net/npm/dill-live2dapi@1.2.7999123456789/
+
+//諾瓦爾"
+//            cdnPath:  "https://gcore.jsdelivr.net/npm/dill-live2dapi@1.2.8123/"
 //成功
 //            cdnPath:  "https://gcore.jsdelivr.net/npm/dill-live2dapi@1.2.7999123456/"
 //            cdnPath: "https://gcore.jsdelivr.net/gh/Akilarlxh/live2d_api@latest/"
