f81b3e4889b72796faca934c2fd31b5daa8a54ad
使用者名稱 <>
2023-11-19 08:30:04 +0500

    Site updated: 2023-11-19 08:30:04

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -61,12 +61,15 @@ if (screen.width >= 0) {
 //            cdnPath:  "https://gcore.jsdelivr.net/npm/dill-live2dapi@latest/"
 //            cdnPath:  "https://gcore.jsdelivr.net/npm/dill-live2dapi@1.1.6/"
 //            cdnPath:  "https://gcore.jsdelivr.net/npm/dill-live2dapi@1.2.4/"
-//            cdnPath:  "https://gcore.jsdelivr.net/npm/dill-live2dapi@1.2.7999123456789/switch_textures/?id=1-18"    
+//            cdnPath:  "https://gcore.jsdelivr.net/npm/dill-live2dapi@1.2.7999123456789/switch_textures/?id=1-18"   
+
+
+            cdnPath:  "https://gcore.jsdelivr.net/npm/dill-live2dapi@1.2.8123456/" 
 //nepnep超次原海王星+ npetune
 //            cdnPath:  "https://gcore.jsdelivr.net/npm/dill-live2dapi@1.2.7999123456789/"
 
 //諾瓦爾+ npetune
-            cdnPath:  "https://gcore.jsdelivr.net/npm/dill-live2dapi@1.2.812345/"
+//            cdnPath:  "https://gcore.jsdelivr.net/npm/dill-live2dapi@1.2.8123/"
 //成功
 //            cdnPath:  "https://gcore.jsdelivr.net/npm/dill-live2dapi@1.2.7999123456/"
 //            cdnPath: "https://gcore.jsdelivr.net/gh/Akilarlxh/live2d_api@latest/"
